96dd1f12defdcb3d0f6859401d105d18aca44ae2
cidborba <cidfernando@gmail.com>
2024-06-23 00:37:53 -0300

    Removendo variáveis inúteis em shared

--- restaurant/shared.py
+++ restaurant/shared.py
@@ -9,21 +9,15 @@ from restaurant.table import Table
 #       global my_global_variable
 #       return my_global_variable
 
-ready_to_order = list()
 max_clients = 0
 crew_attended = 0
-totem_attended = 0
 chef_attended = 0
 lock_totem = Lock()
 new_ticket = Condition()
 lock_crew = Lock()
-new_crew = Condition(lock_crew)
 lock_chef = Lock()
 new_order = Condition()
-lock_table = Lock()
 clients = list()
-client_waiting_crew = list()
 client_waiting_chef = list()
-client_waiting_table = list()
 totem = None
 table = Table(0)
